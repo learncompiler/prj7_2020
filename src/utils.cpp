49cffaf93e358c0c4c4022e2259364d217676ec4
Yu Jian <lkmcfj@outlook.com>
2020-12-10 11:35:23 +0800

    add VerificationError, SyntaxError, print vc

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "log.h"
 #include "action.h"
 
 bool verify_basic_path(std::vector<Action*> basic_path)
@@ -7,6 +8,9 @@ bool verify_basic_path(std::vector<Action*> basic_path)
     for (size_t i = basic_path.size() - 1; i < basic_path.size(); --i)
         basic_path[i]->maintain_vc(vc);
     if (!vc) return true;
+    std::ostringstream builder;
+    vc->to_string(builder);
+    Log::info << "vc: " << builder.str() << '\n';
     ExprContext context;
     z3::expr z3_vc = vc->z3_expr(context);
     z3::solver s(context.z3_context());
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <vector>
+#include <stdexcept>
 #define INT_WIDTH 32
 
 class Action;
 bool verify_basic_path(std::vector<Action*> basic_path);
+class VerificationError : public std::runtime_error
+{
+public:
+    using std::runtime_error::runtime_error;
+};
+class SyntaxError : public std::runtime_error
+{
+public:
+    using std::runtime_error::runtime_error;
+};
