04b906a3f2ac418e77849f75c65fab3abed58039
Yu Jian <lkmcfj@outlook.com>
2020-12-10 11:43:46 +0800

    handle file error, verification error and syntax error in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,23 @@
 #include "mdverifLexer.h"
 #include "mdverifParser.h"
 #include "verif_visitor.h"
+#include "utils.h"
 
 using namespace antlr4;
 using namespace std;
 
 int main(int argc, const char* argv[]) {
     if (argc != 2) {
-        std::cerr << "[error] Input sourcefile required:" << argv[0] << " <sourcefile>\n";
+        cerr << "[error] Input sourcefile required:" << argv[0] << " <sourcefile>\n";
         return 1;
     }
     ifstream sourceFile;
     sourceFile.open(argv[1]);
+    if (!sourceFile.good())
+    {
+        cout << "fail to open source file.";
+        return 0;
+    }
 
     // lexer & parser part of antlr4
     ANTLRInputStream input(sourceFile);
@@ -24,8 +30,20 @@ int main(int argc, const char* argv[]) {
 
     mdverifParser::ProgramContext* root = parser.program();
     VerifVisitor visitor;
-    visitor.visitProgram(root);
-    
+    try
+    {
+        visitor.visitProgram(root);
+    } catch (VerificationError &e)
+    {
+        cout << e.what() << '\n';
+        cout << "Verification failed.\n";
+        return 0;
+    } catch (SyntaxError &e)
+    {
+        cout << e.what() << '\n';
+        cout << "Syntax error.\n";
+        return 0;
+    }
     cout << "Verified.\n";
     return 0;
 }
